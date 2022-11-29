246713d3f328dded54951d4dbb59d9720f7702e4
Nathan Fonteyne <nathanfonteyne@MacBook-Pro-de-Nathan.local>
2022-11-29 18:58:00 +0100

    NFT non echangeable

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -3,9 +3,9 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-//import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract tiketEvent is ERC721, Ownable {
 
